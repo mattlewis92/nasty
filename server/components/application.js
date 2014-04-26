c27c068e99acb361c7b5e3258516dc53552d4499
Matt Lewis <matt@iwaz.at>
2014-04-26 17:02:25 +0100

    refactor so the app now extends express

--- server/components/application.js
+++ server/components/application.js
@@ -8,27 +8,6 @@ var express = require('express')
 
 require('express-di');
 
-var application = function() {
-
-  this.services = dependable.container();
-
-  this.app = express();
-
-  var self = this;
-
-  var originalRegisterFunction = this.services.register;
-  this.services.register = function(key, value) {
-
-    self.app.factory(key, function(req, res, next) {
-      next(null, value);
-    });
-
-    originalRegisterFunction(key, value);
-
-  }
-
-}
-
 var initMiddleware = function(app, services) {
 
   var config = services.get('config');
@@ -79,126 +58,149 @@ var addFinalMiddleware = function(app, services) {
 
 }
 
-application.prototype.loadConfig = function(configPath) {
+var application = function() {
 
-  nconf
-    .overrides({
-      'NODE_ENV': process.env.NODE_ENV || 'development',
-      'rootPath': configPath + '/../../'
-    })
-    .env()
-    .file('all', configPath + '/all.json')
-    .file('other', configPath + '/' + nconf.get('NODE_ENV') + '.json');
+  var app = express();
 
-  this.services.register('config', nconf);
+  app.services = dependable.container();
 
-  initMiddleware(this.app, this.services);
+  var originalRegisterFunction = app.services.register;
+  app.services.register = function(key, value) {
 
-}
+    app.factory(key, function(req, res, next) {
+      next(null, value);
+    });
 
-application.prototype.loadServices = function(servicesPath) {
+    originalRegisterFunction(key, value);
 
-  var services = requireIndex(servicesPath);
+  }
 
-  for (var name in services) {
+  app.loadConfig = function(configPath) {
 
-    if ('object' == typeof services[name] && services[name].index) {
-      services[name] = services[name].index;
+    nconf
+      .overrides({
+        'NODE_ENV': process.env.NODE_ENV || 'development',
+        'rootPath': configPath + '/../../'
+      })
+      .env()
+      .file('all', configPath + '/all.json')
+      .file('other', configPath + '/' + nconf.get('NODE_ENV') + '.json');
+
+    this.services.register('config', nconf);
+
+    initMiddleware(this, this.services);
+
+  }
+
+  app.loadServices = function(servicesPath) {
+
+    var services = requireIndex(servicesPath);
+
+    for (var name in services) {
+
+      if ('object' == typeof services[name] && services[name].index) {
+        services[name] = services[name].index;
+      }
+
+      this.services.register(name, new services[name]());
     }
 
-    this.services.register(name, new services[name]());
   }
 
-}
+  app.loadModules = function(modulePath) {
 
-application.prototype.loadModules = function(modulePath) {
+    var finder = require('findit')(modulePath);
+    var self = this;
 
-  var finder = require('findit')(modulePath);
-  var self = this;
+    function camelCase(input) {
+      return input.toLowerCase().replace(/_(.)/g, function(match, group1) {
+        return group1.toUpperCase();
+      });
+    }
 
-  function camelCase(input) {
-    return input.toLowerCase().replace(/_(.)/g, function(match, group1) {
-      return group1.toUpperCase();
+    finder.on('file', function (file, stat) {
+      var filename = file.split('/').pop();
+      var directory = file.replace('/' + filename, '');
+
+      if ('app.js' == filename) {
+
+        var subApp = express();
+        initMiddleware(subApp, self.services);
+
+        var mountPrefix = directory.replace(modulePath, '');
+        var actions = requireIndex(directory + '/actions');
+
+        if (fs.existsSync(directory + '/middleware')) {
+
+          var middleware = requireIndex(directory + '/middleware');
+
+          for (var name in middleware) {
+            //Make this middleware available locally
+            subApp.factory(name, middleware[name]);
+
+            //Also make the middleware available globally
+            var globalName = mountPrefix.replace(/\//g, '_') + '_' + name;
+            if (globalName.charAt(0) == '_') globalName = globalName.substr(1);
+            globalName = camelCase(globalName);
+
+            self.factory(globalName, middleware[name]);
+          }
+
+        }
+
+        var subAppLoaded = require(file)(subApp, actions);
+
+        addFinalMiddleware(subApp, self.services);
+
+        self.use(mountPrefix, subAppLoaded);
+
+      }
+
     });
-  }
 
-  finder.on('file', function (file, stat) {
-    var filename = file.split('/').pop();
-    var directory = file.replace('/' + filename, '');
+    //Now let's add some default routes (as it's own sub app otherwise they'll override every other route)
+    finder.on('end', function () {
 
-    if ('app.js' == filename) {
+      var config = self.services.get('config');
+      var indexFile = path.resolve(config.get('rootPath') + config.get('frontendPath') + '/index.html');
 
       var subApp = express();
       initMiddleware(subApp, self.services);
 
-      var mountPrefix = directory.replace(modulePath, '');
-      var actions = requireIndex(directory + '/actions');
+      subApp.get('*', function(req, res, next) {
+        res.sendfile(indexFile);
+      });
 
-      if (fs.existsSync(directory + '/middleware')) {
-
-        var middleware = requireIndex(directory + '/middleware');
-
-        for (var name in middleware) {
-          //Make this middleware available locally
-          subApp.factory(name, middleware[name]);
-
-          //Also make the middleware available globally
-          var globalName = mountPrefix.replace(/\//g, '_') + '_' + name;
-          if (globalName.charAt(0) == '_') globalName = globalName.substr(1);
-          globalName = camelCase(globalName);
-
-          self.app.factory(globalName, middleware[name]);
-        }
-
-      }
-
-      var subAppLoaded = require(file)(subApp, actions);
+      subApp.all('*', function(req, res, next) {
+        res.json(404, {error: 'This API method does not exist.'});
+      });
 
       addFinalMiddleware(subApp, self.services);
 
-      self.app.use(mountPrefix, subAppLoaded);
+      self.use('/', subApp);
 
-    }
+      addFinalMiddleware(self, self.services);
 
-  });
-
-  //Now let's add some default routes (as it's own sub app otherwise they'll override every other route)
-  finder.on('end', function () {
-
-    var config = self.services.get('config');
-    var indexFile = path.resolve(config.get('rootPath') + config.get('frontendPath') + '/index.html');
-
-    var subApp = express();
-    initMiddleware(subApp, self.services);
-
-    subApp.get('*', function(req, res, next) {
-      res.sendfile(indexFile);
     });
 
-    subApp.all('*', function(req, res, next) {
-      res.json(404, {error: 'This API method does not exist.'});
+  }
+
+  app.startServer = function(done) {
+
+    var config = this.services.get('config');
+
+    http.createServer(this).listen(config.get('server:port'), config.get('server:address'), function() {
+      var addr = this.address();
+      console.info('HTTP server listening on %s:%d', addr.address, addr.port);
+      return done();
     });
 
-    addFinalMiddleware(subApp, self.services);
+  }
 
-    self.app.use('/', subApp);
-
-    addFinalMiddleware(self.app, self.services);
-
-  });
+  return app;
 
 }
 
-application.prototype.startServer = function(done) {
 
-  var config = this.services.get('config');
-
-  http.createServer(this.app).listen(config.get('server:port'), config.get('server:address'), function() {
-    var addr = this.address();
-    console.info('HTTP server listening on %s:%d', addr.address, addr.port);
-    return done();
-  });
-
-}
 
 module.exports = application;
