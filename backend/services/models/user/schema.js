d7a15ab22a1926a9a6b4145bbe654bc6ea56c4f7
Matt Lewis <matt@iwaz.at>
2014-08-04 21:30:39 +0100

    Capitalize names

--- backend/services/models/user/schema.js
+++ backend/services/models/user/schema.js
@@ -1,11 +1,29 @@
 'use strict';
 
+var toTitleCase = function(str) {
+  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
 module.exports = function(mongoose) {
 
   return {
     name: {
-      first: {type: String, required: true, trim: true},
-      last: {type: String, required: true, trim: true}
+      first: {
+        type: String,
+        required: true,
+        trim: true,
+        set: function(val) {
+          return toTitleCase(val);
+        }
+      },
+      last: {
+        type: String,
+        required: true,
+        trim: true,
+        set: function(val) {
+          return toTitleCase(val);
+        }
+      }
     },
     email: {type: mongoose.SchemaTypes.Email, required: true, unique: true, lowercase: true, trim: true},
     password: {type: String, required: true, select: false}
