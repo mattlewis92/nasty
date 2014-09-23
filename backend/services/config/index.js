8622268fdf02eaa5f932215f69bb0b66e64b7549
Matt Lewis <matt@iwaz.at>
2014-09-23 17:25:58 +0100

    Move config into the config services directory

--- backend/services/config/index.js
+++ backend/services/config/index.js
@@ -8,17 +8,15 @@ module.exports = function(app) {
 
   return function(rootPath) {
 
-    var configPath = rootPath + '/backend/config';
-
     nconf
       .overrides({
         NODE_ENV: process.env.NODE_ENV || 'development',
         rootPath: rootPath,
-        logPath: configPath + '/../../logs/'
+        logPath: rootPath + '/logs/'
       })
       .env()
-      .file('all', configPath + '/all.json')
-      .file('other', configPath + '/' + nconf.get('NODE_ENV') + '.json');
+      .file('all', __dirname + '/all.json')
+      .file('other', __dirname + '/' + nconf.get('NODE_ENV') + '.json');
 
     //Longer stack traces for non production environments
     if ('production' !== nconf.get('NODE_ENV')) {
@@ -26,7 +24,7 @@ module.exports = function(app) {
       bluebird.longStackTraces();
     }
 
-    var packageJson = JSON.parse(fs.readFileSync(nconf.get('rootPath') + 'package.json'));
+    var packageJson = require(nconf.get('rootPath') + 'package.json');
     if (packageJson.promisify) {
       packageJson.promisify.forEach(function(libraryName) {
         var library = require(libraryName);
