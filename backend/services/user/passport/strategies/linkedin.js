183b019467ed67157c5eebccb7e09e75913f8d30
Matt Lewis <matt@iwaz.at>
2014-10-24 15:14:25 +0100

    Add linkedin authentication strategy

---
+++ backend/services/user/passport/strategies/linkedin.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var LinkedInStrategy = require('passport-linkedin').Strategy;
+
+module.exports = function(passport, services) {
+
+  var strategyConfig = services.get('config').get('authenticationStrategies:linkedin:auth');
+
+  passport.use(new LinkedInStrategy(strategyConfig, function(token, tokenSecret, profile, done) {
+
+    profile.url = profile._json.publicProfileUrl;
+
+    done(null, profile, {token: token, token_secret: tokenSecret});
+
+  }));
+
+};
