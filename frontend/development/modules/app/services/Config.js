b88f9f8915e05778585bdb12c9eacb4f114fb325
Matt Lewis <matt@iwaz.at>
2014-10-27 08:39:17 +0000

    Wrap the config request in a timeout so the nasty login hack will work, fml

--- frontend/development/modules/app/services/Config.js
+++ frontend/development/modules/app/services/Config.js
@@ -2,15 +2,17 @@
 
 angular
   .module('nasty.app.services')
-  .factory('Config', function($translate, HTTP) {
+  .factory('Config', function($timeout, $translate, HTTP) {
 
     var config = {};
 
-    HTTP.GET('app/info').then(function(result) {
-      for (var key in result.data) {
-        config[key] = result.data[key];
-      }
-      $translate.fallbackLanguage(config.i18n.default);
+    $timeout(function() {
+      HTTP.GET('app/info').then(function(result) {
+        for (var key in result.data) {
+          config[key] = result.data[key];
+        }
+        $translate.fallbackLanguage(config.i18n.default);
+      });
     });
 
     return config;
