438ead289a18f398c035e152d8a5019a35e3afb0
Matt Lewis <matt@iwaz.at>
2014-10-28 14:00:14 +0000

    Remove unrequired link function

--- frontend/development/modules/core/directives/loadingBox.js
+++ frontend/development/modules/core/directives/loadingBox.js
@@ -8,8 +8,8 @@ angular
       restrict: 'EA',
       transclude: true,
       templateUrl: 'modules/core/views/directives/loadingBox.html',
-      link: function(scope) {
-        scope.loading = loading;
+      controller: function($scope) {
+        $scope.loading = loading;
       }
     };
 
