b187b5a70eb079c1b37ff0ce7d3971d8cad729da
Matt Lewis <matt@iwaz.at>
2014-11-16 16:48:47 +0000

    Add caching support to the HTTPFactory

--- frontend/development/app/core/services/HTTPFactory.js
+++ frontend/development/app/core/services/HTTPFactory.js
@@ -2,38 +2,122 @@
 
 angular
   .module('nasty.core.services')
-  .factory('HTTPFactory', function($q, DS, DSHttpAdapter) {
+  .factory('HTTPFactory', function($q, $timeout, DS, DSHttpAdapter, DSCacheFactory) {
 
     return function(HTTPConfig) {
 
       HTTPConfig = HTTPConfig || {};
 
-      var instance = {};
+      var instance = {}, cacheBuffer = {};
 
       instance.HTTP = function(config) {
 
-        var deferred = $q.defer(), httpCallbacks = {};
+        var deferred = $q.defer(),
+            httpCallbacks = {},
+            requestCache = null,
+            cacheKey = null;
 
         config.url = DS.defaults.baseURL + config.url;
 
-        DSHttpAdapter.HTTP(angular.extend(config, HTTPConfig)).then(function(result) {
+        var requestConfig = angular.extend(config, HTTPConfig);
 
-          deferred.resolve(result);
+        //Handle caching options
+        if (requestConfig.method === 'GET' && requestConfig.cache) {
 
-          if (httpCallbacks.success) {
-            httpCallbacks.success.call(null, result.data, result.status, result.headers, result.config);
+          //Create the cache if it doesnt exist
+          requestCache = DSCacheFactory.get(requestConfig.cache);
+          if (!requestCache) {
+            requestCache = DSCacheFactory(requestConfig.cache);
           }
 
-        }).catch(function(result) {
+          //The cache key is the URL (as this is a GET request)
+          cacheKey = config.url;
 
-          deferred.reject(result);
+        }
 
-          if (httpCallbacks.error) {
-            httpCallbacks.error.call(null, result.data, result.status, result.headers, result.config);
+        delete requestConfig.cache;
+
+        //First lookup to see if the object exists in the cache
+        if (requestCache && requestCache.get(cacheKey)) {
+
+          //Resolve the promise on the next digest as it can cause lag issues in the UI
+          $timeout(function() {
+            deferred.resolve(requestCache.get(cacheKey));
+          });
+
+        } else if (cacheKey && cacheBuffer[cacheKey]) {
+
+          //If we want to cache the request and another request has already
+          //been instantiated but not yet resolved, then add this request to a buffer
+          //that will be resolved when the original request completed
+          cacheBuffer[cacheKey].push({
+            deferred: deferred,
+            httpCallbacks: httpCallbacks
+          });
+
+        } else {
+
+          //Start the request buffer if this is the first request
+          if (cacheKey && !cacheBuffer[cacheKey])  {
+            cacheBuffer[cacheKey] = [];
           }
 
-        });
+          DSHttpAdapter.HTTP(requestConfig).then(function(result) {
 
+            //Store in the cache if cache option is set
+            if (requestCache) {
+              requestCache.put(cacheKey, result);
+            }
+
+            //Resolve the original request promise
+            deferred.resolve(result);
+
+            if (httpCallbacks.success) {
+              httpCallbacks.success.call(null, result.data, result.status, result.headers, result.config);
+            }
+
+            //Resolve all subsequent requests for this data that happened in the mean time
+            if (cacheBuffer[cacheKey]) {
+              cacheBuffer[cacheKey].forEach(function(bufferItem) {
+
+                bufferItem.deferred.resolve(angular.copy(result));
+
+                if (bufferItem.httpCallbacks.success) {
+                  bufferItem.httpCallbacks.success.call(null, result.data, result.status, result.headers, result.config);
+                }
+
+              });
+              delete cacheBuffer[cacheKey];
+            }
+
+          }).catch(function(result) {
+
+            //Reject the original request promise
+            deferred.reject(result);
+
+            if (httpCallbacks.error) {
+              httpCallbacks.error.call(null, result.data, result.status, result.headers, result.config);
+            }
+
+            //Reject all requests in the buffer
+            if (cacheBuffer[cacheKey]) {
+              cacheBuffer[cacheKey].forEach(function(bufferItem) {
+
+                bufferItem.deferred.reject(angular.copy(result));
+
+                if (bufferItem.httpCallbacks.error) {
+                  bufferItem.httpCallbacks.error.call(null, result.data, result.status, result.headers, result.config);
+                }
+
+              });
+              delete cacheBuffer[cacheKey];
+            }
+
+          });
+
+        }
+
+        //Add these 2 methods to mimic the behaviour of $http
         deferred.promise.success = function(callback) {
           httpCallbacks.success = callback;
           return deferred.promise;
@@ -47,6 +131,7 @@ angular
         return deferred.promise;
       };
 
+      //Add all methods from the original DSHttpAdapter
       [
         'DEL',
         'GET',
